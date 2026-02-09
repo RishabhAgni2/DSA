1d57e8db7c1c5fea028a025fbf021e53f7430048
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-02-09 20:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (96.25%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open =0,add=0;
+        for(char c:s){
+            if(c=='('){
+                open++;
+            }else{
+                if(open>0){
+                    open--;
+                }else{
+                    add++;
+                }
+            }
+        }
+        return open + add;
+    }
+};
