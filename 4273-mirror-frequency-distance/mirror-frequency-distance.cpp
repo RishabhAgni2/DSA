30d7a2d7f3db3a493a7c6ad6e56393362df1e72a
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-04-05 19:18:18 +0530

    Time: 18 ms (93.86%) | Memory: 58.4 MB (78.75%) - LeetSync

---
+++ 4273-mirror-frequency-distance/mirror-frequency-distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mirrorFrequency(string s) {
+        vector<int> freq(128,0);
+        for(char c:s){
+            freq[c]++;
+        }
+        int ans = 0;
+        for(char c='a';c<='z';c++){
+            char m = 'z'-(c-'a');
+            ans += abs(freq[c]-freq[m]);
+        }
+        for(char c='0';c<='9';c++){
+            char m = '9'-(c-'0');
+            ans += abs(freq[c]-freq[m]);
+        }
+        return ans/2;
+    }
+};
